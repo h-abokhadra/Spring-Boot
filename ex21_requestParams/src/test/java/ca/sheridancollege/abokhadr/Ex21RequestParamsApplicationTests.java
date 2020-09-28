e2cee7c451dfba49bb7d943140a04a9327b79ed0
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2020-09-27 21:45:47 -0400

    Done

--- ex21_requestParams/src/test/java/ca/sheridancollege/abokhadr/Ex21RequestParamsApplicationTests.java
+++ ex21_requestParams/src/test/java/ca/sheridancollege/abokhadr/Ex21RequestParamsApplicationTests.java
@@ -2,7 +2,7 @@ package ca.sheridancollege.abokhadr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-//test1
+
 @SpringBootTest
 class Ex21RequestParamsApplicationTests {
 
