589bae240f64e7fbc6d2470bb10d0ad5b1024f94
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2020-10-13 20:36:24 -0400

    Adding @PostMapping

--- lec42_H2andJDBCTemplate/src/main/java/ca/sheridancollege/abokhadr/database/DatabaseAccess.java
+++ lec42_H2andJDBCTemplate/src/main/java/ca/sheridancollege/abokhadr/database/DatabaseAccess.java
@@ -1,9 +1,14 @@
 package ca.sheridancollege.abokhadr.database;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import ca.sheridancollege.abokhadr.beans.Student;
 
 @Repository
 public class DatabaseAccess {
@@ -18,5 +23,20 @@ public class DatabaseAccess {
 		if (rowsAffected > 0)
 			System.out.println("Inserted student into database.");
 	}
+	public void insertStudent(Student student) {
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+		String query = "INSERT INTO student(name) VALUES (:name)";
+		namedParameters.addValue("name", student.getName());
+		int rowsAffected = jdbc.update(query, namedParameters);
+		if (rowsAffected > 0)
+			System.out.println("Inserted student into database.");
+	}
+	
+	public List<Student> getStudentList() {
+		MapSqlParameterSource namedParameters = new
+		MapSqlParameterSource();
+		String query = "SELECT * FROM student";
+		return jdbc.query(query, namedParameters, new BeanPropertyRowMapper<Student>(Student.class));
+		}
 
 }
