cdf7ee5f354cf1cd0def43d78a8c76f90634cfcb
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2020-11-05 12:24:27 -0500

    Upload Project

---
+++ lec51_JDBCTemplate/src/main/java/ca/sheridancollege/abokhadr/Lec51JdbcTemplateApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.abokhadr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec51JdbcTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec51JdbcTemplateApplication.class, args);
+	}
+
+}
