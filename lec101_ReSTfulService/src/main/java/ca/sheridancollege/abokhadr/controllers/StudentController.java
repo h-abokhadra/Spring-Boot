35be5e59f68b3437d5bb9c8dffefc3509c7cb8ef
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2020-11-24 13:38:44 -0500

    Extra methods

--- lec101_ReSTfulService/src/main/java/ca/sheridancollege/abokhadr/controllers/StudentController.java
+++ lec101_ReSTfulService/src/main/java/ca/sheridancollege/abokhadr/controllers/StudentController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +30,18 @@ public class StudentController {
 	public Student getIndividualStudent(@PathVariable Long id) {
 		return da.findById(id);
 	}
+
+	@PostMapping(consumes = "application/json")
+	public Long postStudent(@RequestBody Student student) {
+		return da.save(student);
+	}
+
+	@PutMapping(consumes = "application/json")
+	public String putStudentCollection(@RequestBody List<Student> studentList) {
+		da.deleteAll();
+		da.saveAll(studentList);
+		return "Total Records: " + da.count();
+	}
+	
+	
 }
--- lec101_ReSTfulService/src/main/java/ca/sheridancollege/abokhadr/database/DatabaseAccess.java
+++ lec101_ReSTfulService/src/main/java/ca/sheridancollege/abokhadr/database/DatabaseAccess.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import ca.sheridancollege.abokhadr.beans.Student;
@@ -21,12 +23,38 @@ public class DatabaseAccess {
 		String query = "SELECT * FROM student";
 		return jdbc.query(query, namedParameters, new BeanPropertyRowMapper<Student>(Student.class));
 	}
-	
+
 	public Student findById(Long id) {
 		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
 		String query = "SELECT * FROM student WHERE id = :id";
 		namedParameters.addValue("id", id);
-		return jdbc.query(query, namedParameters, new
-		BeanPropertyRowMapper<Student>(Student.class)).get(0);
+		return jdbc.query(query, namedParameters, new BeanPropertyRowMapper<Student>(Student.class)).get(0);
+	}
+
+	public Long save(Student student) {
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+		KeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+		String query = "INSERT INTO student(name) VALUES(:name)";
+		namedParameters.addValue("name", student.getName());
+		jdbc.update(query, namedParameters, generatedKeyHolder);
+		return (Long) generatedKeyHolder.getKey();
+	}
+
+	public void deleteAll() {
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+		String query = "DELETE FROM student";
+		jdbc.update(query, namedParameters);
+	}
+
+	public Long count() {
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+		String query = "SELECT count(*) FROM student";
+		return jdbc.queryForObject(query, namedParameters, Long.TYPE);
+	}
+
+	public void saveAll(List<Student> studentList) {
+		for (Student s : studentList) {
+			save(s);
 		}
+	}
 }
