5af9b89c1d05b4bc58c25c1aec5d0e9c5f0ebf40
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2020-11-24 12:59:42 -0500

    Create the Project

---
+++ lec101_ReSTfulService/src/main/java/ca/sheridancollege/abokhadr/Lec101ReSTfulServiceApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.abokhadr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec101ReSTfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec101ReSTfulServiceApplication.class, args);
+	}
+
+}
