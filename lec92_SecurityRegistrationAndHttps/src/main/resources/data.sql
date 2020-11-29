9559246f570554ccfc67a1cc0ce567743faad652
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2020-11-29 13:48:30 -0500

    edits

--- lec92_SecurityRegistrationAndHttps/src/main/resources/data.sql
+++ lec92_SecurityRegistrationAndHttps/src/main/resources/data.sql
@@ -1,3 +1,24 @@
+
+INSERT INTO thread(title, body)
+VALUES ('First Thread: What is your favorite programming language?',
+'With time old programming languages become obsolete while new programming
+				languages are launched, but they never gain traction. A common
+				question amongst beginners (and coders alike) is the programming
+				language they should invest learning in, that is in demand, stable
+				outlook, and plenty of jobs. Each programming language is designed
+				for a specific purpose and has its relative merits and demerits.
+				Also, the answer to the most useful programming language to learn is
+				subjective and depends on your familiarity with coding.
+				Irrespective, the following 3 parameters are essential to narrow a
+				language a choice.');
+
+
+
+
+
+
+
+
 INSERT INTO sec_user(email, encryptedPassword, enabled)
 VALUES ('abokhadr@sheridancollege.ca','$2a$10$1ltibqiyyBJMJQ4hqM7f0OusP6np/IHshkYc4TjedwHnwwNChQZCy', 1 );
 
