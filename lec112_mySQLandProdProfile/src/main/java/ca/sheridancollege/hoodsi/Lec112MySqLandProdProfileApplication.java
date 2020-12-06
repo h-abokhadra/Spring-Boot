596440d2a70841fe146c157bea6146928c835796
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2020-12-05 20:07:42 -0500

    Upload Project

---
+++ lec112_mySQLandProdProfile/src/main/java/ca/sheridancollege/hoodsi/Lec112MySqLandProdProfileApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.hoodsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec112MySqLandProdProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec112MySqLandProdProfileApplication.class, args);
+	}
+
+}
