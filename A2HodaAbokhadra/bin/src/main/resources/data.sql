a5471bd61d604fbe837ff5e0112909e7b113a271
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2020-12-07 09:38:02 -0500

    upload

---
+++ A2HodaAbokhadra/bin/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO REVIEW (name, rating, reviewDateTime, reviewSubject)
+VALUES ('First Cow', 4.5, '2020-10-15 07:45', 'A big-hearted story illustrates how we can find brotherhood in the most unlikely places.');
+
+INSERT INTO REVIEW (name, rating, reviewDateTime, reviewSubject)
+VALUES ('Little Women', 5, '2020-10-15 07:55', 'This beautifully acted, beloved story about sisterhood is exactly the heartfelt and uplifting modern classic moviegoers need.');
+
+INSERT INTO REVIEW (name, rating, reviewDateTime, reviewSubject)
+VALUES ('Bad Boys For Life', 5, '2020-10-15 08:00', 'This third buddy-cop continues with the series same kind of cluttered, noisy filmmaking, but now its accompanied by a sense of pained weariness.');
+
